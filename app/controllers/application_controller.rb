7c4ad3a3116274040ff8dc6e68d7346bad34c6d9
errorseiya <errorseiya@gmail.com>
2020-12-06 23:39:24 +0900

    ユーザー編集機能の実装

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,7 @@
 class ApplicationController < ActionController::Base
   before_action :authenticate_user!
+  
+  # ログインしているか確認、していなければログインページに戻す
   before_action :configure_permitted_parameters, if: :devise_controller?
 
 
