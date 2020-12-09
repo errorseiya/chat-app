786b7433d89114055e5050ea00b1beab750872b4
errorseiya <errorseiya@gmail.com>
2020-12-09 16:03:18 +0900

    結合テストの実装

--- spec/factories/messages.rb
+++ spec/factories/messages.rb
@@ -7,5 +7,11 @@ FactoryBot.define do
     after(:build) do |message|
       message.image.attach(io: File.open('public/images/test_image.png'), filename: 'test_image.png')
     end
+    # ↑クラスメソッド定義して、system/users_spec.rbの
+
+    # 添付する画像を定義する
+    # image_path = Rails.root.join('public/images/test_image.png')で↑の”値”が帰ってくるイメージ
+
+    # attachでつなげている
   end
 end
